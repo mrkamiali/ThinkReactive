108c202cc96acbb08ea28bc90494ab3dec52c400
Kamran Ali <kamiali63@gmail.com>
2017-02-05 04:13:17 +0500

    RxAndroid + Retrofit2 with multiple Request .

---
+++ ThinkDagger/app/src/main/java/com/kamran/thinkdagger/AppController.java
@@ -0,0 +1,8 @@
+package com.kamran.thinkdagger;
+
+/**
+ * Created by Kamran ALi on 2/5/2017.
+ */
+
+public class AppController {
+}
--- ThinkDagger/app/src/main/java/com/kamran/thinkdagger/ui/home/MainActivity.java
+++ ThinkDagger/app/src/main/java/com/kamran/thinkdagger/ui/home/MainActivity.java
@@ -1,7 +1,9 @@
-package com.kamran.thinkdagger;
+package com.kamran.thinkdagger.ui.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kamran.thinkdagger.R;
 
 public class MainActivity extends AppCompatActivity {
 
