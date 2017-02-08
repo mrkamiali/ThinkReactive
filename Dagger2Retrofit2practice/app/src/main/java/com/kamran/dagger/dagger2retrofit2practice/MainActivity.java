fe30d95abcb1a5177a7cf68761f4c426af942522
Kamran Ali <kamiali63@gmail.com>
2017-02-08 22:09:27 +0500

    initial commit DAGGER THUG.

---
+++ Dagger2Retrofit2practice/app/src/main/java/com/kamran/dagger/dagger2retrofit2practice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kamran.dagger.dagger2retrofit2practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
